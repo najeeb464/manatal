38632187b00f3e962fb7b1b1e20b2c5332ae19ef
najeeb464 <najeebshah052@gmail.com>
2022-04-29 06:02:58 +0500

    heroku settings removed from local env

--- studentcms/settings.py
+++ studentcms/settings.py
@@ -151,5 +151,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-if DEBUG ==True:
-    django_heroku.settings(locals())
+# if DEBUG ==False:
+#     django_heroku.settings(locals())
